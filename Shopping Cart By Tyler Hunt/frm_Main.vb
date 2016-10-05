68a37cbbef989b33672e01547255fa34e252b8a5
Tyler Hunt <codestack82@gmail.com>
2016-10-05 16:54:20 -0500

    Update

--- Shopping Cart By Tyler Hunt/frm_Main.vb
+++ Shopping Cart By Tyler Hunt/frm_Main.vb
@@ -1,14 +1,22 @@
 ﻿Public Class frm_Main
-    Private Sub frm_Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
 
     Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
         frm_About.ShowDialog()
     End Sub
 
     Private Sub PrintBooksToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrintBooksToolStripMenuItem.Click
-        frm_PrintBooks.ShowDialog()
+        'frm_PrintBooks.ShowDialog()
+
+        'Dim printForm As New frm_PrintBooks
+        'printForm.ShowDialog(Me)
+
+        'Dim index = Me.lb_SelectedList.SelectedIndex
+        ''printForm.Close()
+
+        'MsgBox("Index is :" & index)
+
+        frm_PrintBooks.Show()
     End Sub
 
     Private Sub AudioBooksToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AudioBooksToolStripMenuItem.Click
@@ -19,4 +27,7 @@
         Me.Close()
     End Sub
 
+    Private Sub frm_Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
